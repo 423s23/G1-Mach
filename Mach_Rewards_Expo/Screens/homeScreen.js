8a26700dceaa811d75068c3e0a95af47752cceff
unknown <dlkl46.bl@gmail.com>
2023-03-07 14:29:48 -0700

    progress bar & level

--- Mach_Rewards_Expo/Screens/homeScreen.js
+++ Mach_Rewards_Expo/Screens/homeScreen.js
@@ -1,4 +1,4 @@
-import {Button, Pressable, ScrollView, Text, View} from "react-native";
+import {Button, Pressable, ScrollView, Text, View, StlyeSheet} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import styles from "../Styles/styles";
@@ -8,7 +8,11 @@ import * as React from "react";
 
 function HomeScreen({ navigation }) {
     const starSize = 30;
-    let Level = 1;
+    let Name = "Dylan";
+    let Level = 13;
+    let CurrentPoints = 2300;
+    let ProgressPoints = 3000;
+    let Rank = 1;
     let Titles = [
         "Not Yet Verified",
         "Mach Badass",
@@ -19,37 +23,46 @@ function HomeScreen({ navigation }) {
     return (
         <ScrollView>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginVertical: 20}}>
+                <Text style={homeScreenStyles.nameText}>Hello {[Name]},</Text>
                 <View style={homeScreenStyles.levelBox}>
-                    <Text style={styles.headerText}>{Titles[Level]}</Text>
+                    <Text style={styles.headerText}>{Titles[Rank]}</Text>
                     <View style={homeScreenStyles.starBox}>
                         <Ionicons
-                            name={Level>=1 ? 'ios-star' : 'ios-star-outline'}
+                            name={Rank>=1 ? 'ios-star' : 'ios-star-outline'}
                             size={starSize}
                             style={homeScreenStyles.star}
                         />
                         <Ionicons
-                            name={Level>=2 ? 'ios-star' : 'ios-star-outline'}
+                            name={Rank>=2 ? 'ios-star' : 'ios-star-outline'}
                             size={starSize}
                             style={homeScreenStyles.star}
                         />
                         <Ionicons
-                            name={Level>=3 ? 'ios-star' : 'ios-star-outline'}
+                            name={Rank>=3 ? 'ios-star' : 'ios-star-outline'}
                             size={starSize}
                             style={homeScreenStyles.star}
                         />
                         <Ionicons
-                            name={Level>=4 ? 'ios-star' : 'ios-star-outline'}
+                            name={Rank>=4 ? 'ios-star' : 'ios-star-outline'}
                             size={starSize}
                             style={homeScreenStyles.star}
                         />
                         <Ionicons
-                            name={Level>=5 ? 'ios-star' : 'ios-star-outline'}
+                            name={Rank>=5 ? 'ios-star' : 'ios-star-outline'}
                             size={starSize}
                             style={homeScreenStyles.star}
                         />
                     </View>
-
                 </View>
+                <View style={homeScreenStyles.levelingBox}>
+                    <Text style={homeScreenStyles.levelText}>Level {[Level]}</Text>
+                    <Text style={homeScreenStyles.pointText}>{[CurrentPoints]}/{[ProgressPoints]}</Text>
+                </View>
+                <View style={homeScreenStyles.progressBox}>
+                    <View style={homeScreenStyles.progressBarBackground}></View>
+                    <View style={homeScreenStyles.progressBar}></View>
+                </View>
+                
                 <Pressable style={styles.loginButton} onPress={() => navigation.navigate("Login")}>
                     <Text style={styles.buttonText}>Log In</Text>
                 </Pressable>
--- Mach_Rewards_Expo/Styles/homeScreenStyles.js
+++ Mach_Rewards_Expo/Styles/homeScreenStyles.js
@@ -22,10 +22,63 @@ const homeScreenStyles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'column'
     },
+    nameText:{
+        fontSize: 28,
+        color: '#6ccff6',
+        fontWeight: 400,
+        textAlign: "left",
+        marginRight: 250,
+
+    },
+    levelingBox:{
+        flex: .1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    levelText:{
+        fontSize: 24,
+        color: '#6ccff6',
+        fontWeight: 400,
+        textAlign: 'left',
+        marginRight: 225,
+    },
+    pointText:{
+        fontSize: 20,
+        color: '#6ccff6',
+        fontWeight: 300,
+        textAlign: 'right',
+        bottom: 0,
+    },
+    progressBox:{
+        marginVertical: 20,
+        flex: .2,
+        width: '80%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    progressBarBackground:{
+        position: 'absolute',
+        height: 20,
+        width: '100%',
+        backgroundColor: '#ffffff',
+        borderColor: '#000000',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+    progressBar:{
+        justifyContent: 'left',
+        alignItems: 'left',
+        height: 20,
+        width: '50%',
+        backgroundColor: '#6ccff6',
+        borderColor: '#000000',
+        borderWidth: 2,
+        borderRadius: 5,
+        marginRight: 200,
+    },
     star:{
         margin:10
-    }
-
+    },
 });
 
 export default homeScreenStyles;
