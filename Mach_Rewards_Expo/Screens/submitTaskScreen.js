a117fdd858b819ce852baa267cfcebb9c2a7fe71
unknown <dlkl46.bl@gmail.com>
2023-03-09 10:12:49 -0700

    initial SubmitTaskScreen

--- Mach_Rewards_Expo/Screens/submitTaskScreen.js
+++ Mach_Rewards_Expo/Screens/submitTaskScreen.js
@@ -1,10 +1,33 @@
-import {Text, View} from "react-native";
+import {Button, Pressable, ScrollView, Text, View, StlyeSheet} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import styles from "../Styles/styles";
+import submitTaskScreenStyles from "../Styles/submitTaskScreenStyles.js";
+import {Ionicons} from "@expo/vector-icons";
+import * as React from "react";
+import { TextInput } from "react-native-web";
 
 function SubmitTaskScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>SubmitTaskScreen!</Text>
-        </View>
+        <ScrollView>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <View style={submitTaskScreenStyles.headerText}>
+
+                </View>
+                <TextInput
+
+                />
+                <TextInput
+
+                />
+                <Pressable style={submitTaskScreenStyles.uploadImage} onPress={() => navigation.navigate("SubmitTask")}>
+
+                </Pressable>
+                <Pressable style={submitTaskScreenStyles.submitButton} onPress={() => navigation.navigate("SubmitTask")}>
+                    
+                </Pressable>
+            </View>
+        </ScrollView>
     );
 }
 
---
+++ Mach_Rewards_Expo/Styles/submitTaskScreenStyles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native";
+
+const submitTaskScreenStyles = StyleSheet.create({
+    headerText:{
+
+    },
+    selectTask:{
+
+    },
+    insertText:{
+
+    },
+    uploadImage:{
+
+    },
+    submitButton:{
+
+    },
+});
+
+export default submitTaskScreenStyles;
