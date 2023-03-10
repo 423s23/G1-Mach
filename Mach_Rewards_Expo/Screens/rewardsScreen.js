3c702d842382657f2c6e26804321cd347d7b0355
LukasB4 <lukas@villabernard.com>
2023-03-10 10:21:46 -0700

    Removed unused import

--- Mach_Rewards_Expo/Screens/rewardsScreen.js
+++ Mach_Rewards_Expo/Screens/rewardsScreen.js
@@ -1,6 +1,5 @@
 import {AppRegistry, Image, Button, Pressable, ScrollView, Text, View, StyleSheet} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
-import ProgressCircle from 'react-native-progress-circle'
 import styles from "../Styles/styles";
 
 
